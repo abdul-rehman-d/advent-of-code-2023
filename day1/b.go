86d89071e2a42a99140ba68c2d8d7cdcc87989ea
Abdul Rehman <ar618211@gmail.com>
2023-12-13 20:11:29 +0500

    starting part b of day 1

--- day1/b.go
+++ day1/b.go
@@ -1,5 +1,31 @@
 package day1
 
+import "strings"
+
+func extractDigit(line string) int {
+	days := []string{
+		"one",
+		"two",
+		"three",
+		"four",
+		"five",
+		"six",
+		"seven",
+		"eight",
+		"nine",
+	}
+
+	return len(days)
+}
+
 func PartB(data string) int {
-	return 0
+	lines := strings.Split(string(data), "\n")
+
+	total := 0
+
+	for _, line := range lines {
+		total += len(line) // dummay
+	}
+
+	return total
 }
