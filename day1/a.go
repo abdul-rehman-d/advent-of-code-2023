6cde8aead71579f35b96d0d5bc3a2f66cf74c93b
Abdul Rehman <ar618211@gmail.com>
2023-12-13 11:59:21 -0800

    refactored part a day 1

--- day1/a.go
+++ day1/a.go
@@ -11,25 +11,33 @@ func isDigit(char rune) bool {
 	return ascii < 58 && ascii > 47
 }
 
-func getTwoDigits(s string) int {
-	var first rune
-	var second rune
-
+func getFirstDigit(s string) int {
 	for i := 0; i < len(s); i++ {
 		char := rune(s[i])
 		if isDigit(char) {
-			first = char
-			break
+			val, _ := strconv.Atoi(string(char))
+			return val
 		}
 	}
 
+	return 0
+}
+
+func getLastDigit(s string) int {
 	for i := len(s) - 1; i >= 0; i-- {
 		char := rune(s[i])
 		if isDigit(char) {
-			second = char
-			break
+			val, _ := strconv.Atoi(string(char))
+			return val
 		}
 	}
+
+	return 0
+}
+
+func getTwoDigits(s string) int {
+	first := getFirstDigit(s)
+	second := getLastDigit(s)
 
 	val, _ := strconv.Atoi(fmt.Sprintf("%c%c", first, second))
 	return val
