4a58c2bac5f523a62851bddb20a05cda9b209daa
Abdul Rehman <ar618211@gmail.com>
2023-12-15 19:08:27 -0800

    part b of day 2 done

--- day2/b.go
+++ day2/b.go
@@ -1,5 +1,28 @@
 package day2
 
+import "strings"
+
 func PartB(data string) int {
-	return 0
+	lines := strings.Split(data, "\n")
+
+	sum := 0
+
+	for _, line := range lines {
+		game := getGame(line)
+		for _, sets := range game {
+			min := make(map[string]int, 2)
+			min["red"] = 0
+			min["green"] = 0
+			min["blue"] = 0
+			for _, set := range sets {
+				for color, amount := range set {
+					if amount > min[color] {
+						min[color] = amount
+					}
+				}
+			}
+			sum += (min["red"] * min["green"] * min["blue"])
+		}
+	}
+	return sum
 }
