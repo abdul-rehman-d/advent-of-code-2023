717d81d06ce66a32f19af3f2cd305b9a88c3903c
Abdul Rehman <ar618211@gmail.com>
2023-12-26 14:34:55 +0500

    generate cmd done

--- main.go
+++ main.go
@@ -18,7 +18,95 @@ func getChallengeInput(i int) string {
 	return string(arrByte)
 }
 
-func make(day int) {}
+func getMainFileContents(day int, part string) string {
+	return fmt.Sprintf(`package day%d
+
+func Part%s(data string) int {
+	return 0
+}
+`, day, part)
+}
+
+func getTestFileContents(day int) string {
+	return fmt.Sprintf(`package day%d
+
+import (
+	"testing"
+)
+
+func TestPartA(t *testing.T) {
+	data := ""
+
+	expected := 0
+	result := PartA(data)
+
+	if expected != result {
+		t.Fatalf("\nExpected = %%d\nResult = %%d\n", expected, result)
+	}
+}
+
+
+func TestPartB(t *testing.T) {
+	data := ""
+
+	expected := 0
+	result := PartB(data)
+
+	if expected != result {
+		t.Fatalf("\nExpected = %%d\nResult = %%d\n", expected, result)
+	}
+}
+`, day)
+}
+
+func generate(day int) error {
+	folderName := fmt.Sprintf("./day%d", day)
+
+	if s, err := os.Stat(folderName); !os.IsNotExist(err) && s != nil {
+		return fmt.Errorf("already exists")
+	}
+
+	err := os.Mkdir(folderName, 0755)
+	if err != nil {
+		return err
+	}
+
+	var f *os.File
+
+	f, err = os.Create(fmt.Sprintf("%s/a.go", folderName))
+	if err != nil {
+		return err
+	}
+	f.WriteString(getMainFileContents(day, "A"))
+
+	f, err = os.Create(fmt.Sprintf("%s/b.go", folderName))
+	if err != nil {
+		return err
+	}
+	f.WriteString(getMainFileContents(day, "B"))
+
+	_, err = os.Create(fmt.Sprintf("%s/input.txt", folderName))
+	if err != nil {
+		return err
+	}
+
+	_, err = os.Create(fmt.Sprintf("%s/questionA.md", folderName))
+	if err != nil {
+		return err
+	}
+	_, err = os.Create(fmt.Sprintf("%s/questionB.md", folderName))
+	if err != nil {
+		return err
+	}
+
+	f, err = os.Create(fmt.Sprintf("%s/day%d_test.go", folderName, day))
+	if err != nil {
+		return err
+	}
+	f.WriteString(getTestFileContents(day))
+
+	return nil
+}
 
 func solve(day int) {
 
@@ -38,7 +126,7 @@ func main() {
 		fmt.Println(`Usage ./aoc [cmd] [day]
 Available commands:-
 - solve
-- make`)
+- generate`)
 		os.Exit(1)
 	}
 
@@ -56,8 +144,12 @@ Available commands:-
 	switch os.Args[1] {
 	case "solve":
 		solve(day)
-	case "make":
-		make(day)
+	case "generate":
+		err := generate(day)
+		if err != nil {
+			fmt.Println(err)
+			os.Exit(1)
+		}
 	default:
 		fmt.Println("Invalid command.")
 		os.Exit(1)
