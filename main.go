6b11bd99fa61e6fbc080972bf92350979fee7cf5
Abdul Rehman <ar618211@gmail.com>
2023-12-26 13:56:34 +0500

    made main into cli thingy to generate days

--- main.go
+++ main.go
@@ -4,6 +4,7 @@ import (
 	"fmt"
 	"log"
 	"os"
+	"strconv"
 
 	"github.com/abdul-rehman-d/advent-of-code-2023/day1"
 	"github.com/abdul-rehman-d/advent-of-code-2023/day2"
@@ -17,23 +18,12 @@ func getChallengeInput(i int) string {
 	return string(arrByte)
 }
 
-func main() {
+func make(day int) {}
+
+func solve(day int) {
 
 	partA := []func(string) int{day1.PartA, day2.PartA}
 	partB := []func(string) int{day1.PartB, day2.PartB}
-
-	fmt.Print("Enter the day you want to run: ")
-
-	var day int
-	_, err := fmt.Scan(&day)
-
-	if err != nil {
-		panic(err)
-	}
-
-	if day <= 0 {
-		log.Fatalln("Day cannot be less than 1")
-	}
 
 	if day > len(partA) {
 		log.Fatalf("Day cannot be more than %d\n", len(partA))
@@ -41,4 +31,35 @@ func main() {
 
 	fmt.Printf("Part A Answer => %d\n", partA[day-1](getChallengeInput(day)))
 	fmt.Printf("Part B Answer => %d\n", partB[day-1](getChallengeInput(day)))
+}
+
+func main() {
+	if len(os.Args) != 3 {
+		fmt.Println(`Usage ./aoc [cmd] [day]
+Available commands:-
+- solve
+- make`)
+		os.Exit(1)
+	}
+
+	day, err := strconv.Atoi(os.Args[2])
+	if err != nil {
+		fmt.Printf("Error parsing day %s\n", os.Args[2])
+		os.Exit(1)
+	}
+
+	if day <= 0 {
+		fmt.Println("Day cannot be less than 1")
+		os.Exit(1)
+	}
+
+	switch os.Args[1] {
+	case "solve":
+		solve(day)
+	case "make":
+		make(day)
+	default:
+		fmt.Println("Invalid command.")
+		os.Exit(1)
+	}
 }
