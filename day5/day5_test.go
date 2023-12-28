e016a4765d06dc64ec8e1c203272a8c0b19167c2
Abdul Rehman <ar618211@gmail.com>
2023-12-28 11:54:06 +0500

    initialized day5

---
+++ day5/day5_test.go
@@ -0,0 +1,59 @@
+package day5
+
+import (
+	"testing"
+)
+
+func TestPartA(t *testing.T) {
+	data := `seeds: 79 14 55 13
+
+seed-to-soil map:
+50 98 2
+52 50 48
+
+soil-to-fertilizer map:
+0 15 37
+37 52 2
+39 0 15
+
+fertilizer-to-water map:
+49 53 8
+0 11 42
+42 0 7
+57 7 4
+
+water-to-light map:
+88 18 7
+18 25 70
+
+light-to-temperature map:
+45 77 23
+81 45 19
+68 64 13
+
+temperature-to-humidity map:
+0 69 1
+1 0 69
+
+humidity-to-location map:
+60 56 37
+56 93 4`
+
+	expected := 35
+	result := PartA(data)
+
+	if expected != result {
+		t.Fatalf("\nExpected = %d\nResult = %d\n", expected, result)
+	}
+}
+
+func TestPartB(t *testing.T) {
+	data := ""
+
+	expected := 0
+	result := PartB(data)
+
+	if expected != result {
+		t.Fatalf("\nExpected = %d\nResult = %d\n", expected, result)
+	}
+}
