db234f4f5303a317fa653c50aabc260f091f5322
Abdul Rehman <ar618211@gmail.com>
2023-12-28 11:50:45 +0500

    day 4 part b done

--- day4/b.go
+++ day4/b.go
@@ -1,5 +1,40 @@
 package day4
 
+import "strings"
+
 func PartB(data string) int {
-	return 0
+	lines := strings.Split(data, "\n")
+
+	nums := make([]int, len(lines))
+
+	for idx, line := range lines {
+		nums[idx] += 1
+
+		_, after, found := strings.Cut(line, ":")
+		if !found {
+			continue
+		}
+		winning, have, _ := strings.Cut(after, "|")
+		winningNumbers, _ := parseIntArr(winning)
+		haveNumbers, _ := parseIntArr(have)
+
+		score := 0
+
+		for num, _ := range winningNumbers {
+			if haveNumbers[num] != 0 {
+				score++
+			}
+		}
+
+		for i := 1; i <= score; i++ {
+			nums[idx+i] += nums[idx]
+		}
+
+	}
+
+	total := 0
+	for _, num := range nums {
+		total += num
+	}
+	return total
 }
